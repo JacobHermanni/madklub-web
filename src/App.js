8488098bd22cabf9f7185d7c246c6a02ffccc95b
JacobHermanni <hijakker123@hotmail.com>
2018-09-23 17:11:11 +0200

    with cors, google-spreadsheet can fetch from sheet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,84 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var GoogleSpreadsheet = require('google-spreadsheet');
+var async = require('async');
+
+// spreadsheet key is the long id in the sheets URL
+var doc = new GoogleSpreadsheet('166fSi7fmm7yeYSMVjvCrMp1DIoZLxn3vIKjQO9EjKCE');
+var sheet;
+
+async.series([
+  function useServiceAcc(step) {
+    // see notes below for authentication instructions!
+    var creds = require('./credentials/MadklubApp-d7dc96a6b3ba.json');
+    // OR, if you cannot save the file locally (like on heroku)   
+
+    doc.useServiceAccountAuth(creds, step);
+  },
+  function getInfoAndWorksheets(step) {
+    console.log(doc);
+    doc.getInfo(function(err, info) {
+      console.log(info);
+      console.log('Loaded doc: '+info.title+' by '+info.author.email);
+      sheet = info.worksheets[0];
+      console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+      step();
+    });
+  },
+  function workingWithRows(step) {
+    // google provides some query options
+    sheet.getRows({
+      offset: 1,
+      limit: 20,
+      orderby: 'col2'
+    }, function( err, rows ){
+      console.log('Read '+rows.length+' rows');
+      console.log("rows:", rows);
+      // the row is an object with keys set by the column headers
+      
+      step();
+    });
+  },
+  function workingWithCells(step) {
+    sheet.getCells({
+      'min-row': 1,
+      'max-row': 5,
+      'return-empty': true
+    }, function(err, cells) {
+      var cell = cells[0];
+      console.log('from cells[0]: Cell.row: '+cell.row+' cell.col:'+cell.col+' cell.value: '+cell.value);
+
+      // cells have a value, numericValue, and formula
+      cell.value == '1'
+      cell.numericValue == 1;
+      cell.formula == '=ROW()';
+
+      // updating `value` is "smart" and generally handles things for you
+      cell.value = 123;
+      cell.value = '=A1+B2'
+      
+
+      // bulk updates make it easy to update many cells at once
+      cells[0].value = 1;
+      cells[1].value = 2;
+      cells[2].formula = '=A1+B1';
+      
+
+      step();
+    });
+  },   
+  
+], function(err){
+    if( err ) {
+      console.log('Error: '+err);
+    }
+});
+
 
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +86,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Tos get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
