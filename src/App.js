e3150953cde9538c4bd8b231e13ed6a3a216e925
JacobHermanni <hijakker123@hotmail.com>
2018-09-26 17:15:12 +0200

    env testing

--- src/App.js
+++ src/App.js
@@ -1,98 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-var GoogleSpreadsheet = require('google-spreadsheet');
-var async = require('async');
-const netlifyIdentity = require("netlify-identity-widget");
-
-
-netlifyIdentity.init({});
-
-netlifyIdentity.open(); // open the modal
-netlifyIdentity.open("login"); // open the modal to the login tab
-netlifyIdentity.on("login", user => console.log(user));
-
-// spreadsheet key is the long id in the sheets URL
-var doc = new GoogleSpreadsheet('166fSi7fmm7yeYSMVjvCrMp1DIoZLxn3vIKjQO9EjKCE');
-var sheet;
-
-async.series([
-  function useServiceAcc(step) {
-    // see notes below for authentication instructions!
-    try {
-      var creds = require('./credentials/client.json');
-      // OR, if you cannot save the file locally (like on heroku)   
-
-      doc.useServiceAccountAuth(creds, step);
-    } catch (e) { }
-  },
-  function getInfoAndWorksheets(step) {
-    doc.getInfo(function (err, info) {
-      console.log(info);
-      console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
-      sheet = info.worksheets[0];
-      console.log('sheet  1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
-      step();
-    });
-  },
-  function workingWithRows(step) {
-    // google provides some query options
-    sheet.getRows({
-      offset: 1,
-      limit: 20,
-      orderby: 'col2'
-    }, function (err, rows) {
-      console.log('Read ' + rows.length + ' rows');
-      console.log("rows:", rows);
-      // the row is an object with keys set by the column headers
-
-      step();
-    });
-  },
-  function workingWithCells(step) {
-    sheet.getCells({
-      'min-row': 6,
-      'max-row': 6,
-      'return-empty': true
-    }, function (err, cells) {
-      var cell = cells[0];
-      console.log('from cells[0]: Cell.row: ' + cell.row + ' cell.col:' + cell.col + ' cell.value: ' + cell.value);
-
-
-      // cells have a value, numericValue, and formula
-      cell.value == 2;
-      //cell.numericValue == 1;
-      //cell.formula == '=ROW()';
-      cell.setValue("2aanew", (callback) => console.log(callback));
-      console.log("gets here after value change?");
-
-      // updating `value` is "smart" and generally handles things for you
-      // cell.value = 123;
-      // cell.value = '=A1+B2'
-
-
-      // bulk updates make it easy to update many cells at once
-      // cells[0].value = 1;
-      // cells[1].value = 2;
-      // cells[2].formula = '=A1+B1';
-
-
-      step();
-    });
-  },
-
-], function (err) {
-  if (err) {
-    console.log('Error: ' + err);
-  }
-});
-
 
 class App extends Component {
   render() {
 
     return (
       <div className="App">
+        {console.log("env")}
+        {console.log(process.env.NODE_ENV)}
+        {console.log("process.env", process.env)}
+        {console.log("process.env.REACT_APP_KEY", process.env.REACT_APP_KEY)}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -100,6 +18,8 @@ class App extends Component {
         <p className="App-intro">
           Tos get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <span>current env: {process.env.NODE_ENV}</span>
+        <span>key: {process.env.REACT_APP_KEY}</span>
       </div>
     );
   }
